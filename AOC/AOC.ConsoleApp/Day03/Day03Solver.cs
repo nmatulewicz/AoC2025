077f768d6d32b8bf4a998bb54514ca1e48a8cf3a
Nadia <nadia.matulewicz@gmail.com>
2025-12-03 20:09:01 +0100

    solve day 3 part 1

---
+++ AOC/AOC.ConsoleApp/Day03/Day03Solver.cs
@@ -0,0 +1,38 @@
+﻿namespace AOC.ConsoleApp.Day03;
+
+/// <summary>
+/// Part 1: 16946
+/// </summary>
+
+internal class Day03Solver : ISolver
+{
+    private readonly IEnumerable<string> _banks;
+    public Day03Solver (IEnumerable<string> lines)
+    {
+        _banks = lines;
+    }
+
+    public string SolveFirstChallenge()
+    {
+        return _banks.Sum(GetLargestPossibleJoltage).ToString();
+    }
+
+    public string SolveSecondChallenge()
+    {
+        throw new NotImplementedException();
+    }
+
+    private int GetLargestPossibleJoltage(string bank)
+    {
+        var firstJoltageDigit = GetGreatestDigit(bank, startIndex: 0, endIndex: bank.Length - 1);
+        var firstOccurranceOfFirstDigit = bank.IndexOf(firstJoltageDigit);
+        var secondJoltageDigit = GetGreatestDigit(bank, startIndex: firstOccurranceOfFirstDigit + 1, bank.Length);
+        return int.Parse("" + firstJoltageDigit + secondJoltageDigit);
+    }
+
+    private static char GetGreatestDigit(string bank, int startIndex, int endIndex)
+    {
+        var substring = bank.Substring(startIndex, endIndex - startIndex);
+        return substring.MaxBy(digitString => int.Parse("" + digitString));
+    }
+}
--- AOC/AOC.ConsoleApp/Program.cs
+++ AOC/AOC.ConsoleApp/Program.cs
@@ -1,10 +1,10 @@
 ﻿
-using AOC.ConsoleApp.Day02;
+using AOC.ConsoleApp.Day03;
 
-var lines = ReadLines("../../../Day02/input.txt");
-//var lines = ReadLines("../../../Day02/input_small.txt");
+var lines = ReadLines("../../../Day03/input.txt");
+//var lines = ReadLines("../../../Day03/input_small.txt");
 
-ISolver solver = new Day02Solver(lines);
+ISolver solver = new Day03Solver(lines);
 
 Console.WriteLine($"Solution 1: {solver.SolveFirstChallenge()}");
 Console.WriteLine($"Solution 2: {solver.SolveSecondChallenge()}");
