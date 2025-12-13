11b1271e4437f14d669c4d49302fa1bf6be84734
Nadia <nadia.matulewicz@gmail.com>
2025-12-13 22:36:12 +0100

    solve day 7 part 2

--- AOC/AOC.ConsoleApp/Day07/Day07Solver.cs
+++ AOC/AOC.ConsoleApp/Day07/Day07Solver.cs
@@ -4,16 +4,18 @@ namespace AOC.ConsoleApp.Day07;
 
 /// <summary>
 /// Solution 1: 1537
-/// Solution 2:
+/// Solution 2: 18818811755665
 /// </summary> 
 
 internal class Day07Solver : ISolver
 {
     private readonly Grid _grid;
+    private readonly Dictionary<GridPosition<char>, UInt128> _timelinesPerPosition;
 
     public Day07Solver(IEnumerable<string> lines)
     {
         _grid = new Grid(lines);
+        _timelinesPerPosition = [];
     }
 
     public string SolveFirstChallenge()
@@ -30,7 +32,12 @@ internal class Day07Solver : ISolver
 
     public string SolveSecondChallenge()
     {
-        return "";
+        //var splittersFromBottomToTop = _grid.Where(IsSplitter).Reverse();
+        //foreach (var splitter in splittersFromBottomToTop)
+        //{
+        //    DetermineNumberOfDifferentTimelines(splitter);
+        //}
+        return DetermineNumberOfDifferentTimelines(_grid.First(IsSplitter)).ToString();
     }
 
     public static bool IsStart(GridPosition<char> position)
@@ -74,5 +81,62 @@ internal class Day07Solver : ISolver
             TrySendBeam(position.GetNeighbour(0, -1));
             TrySendBeam(position.GetNeighbour(0, 1));
         }
+    }
+
+    public UInt128 DetermineNumberOfDifferentTimelines(GridPosition<char> position)
+    {
+        //if (!position.IsValidPosition) throw new InvalidOperationException(
+        //    "Cannot determine number of different lifetimes for invalid position.");
+
+        if (!IsSplitter(position) && !IsStart(position)) throw new ArgumentException(
+            "cannot determine number of timelines of non-splitter position.");
+
+        if (_timelinesPerPosition.TryGetValue(position, out var numberOfTimelines)) return numberOfTimelines;
+
+        // determine number of timelines on the left
+        var leftNeighbour = position.GetNeighbour(0, -1);
+        var next = leftNeighbour;
+        UInt128 lifetimesLeft;
+        while (true)
+        {
+            if (!next.IsValidPosition) {
+                lifetimesLeft = 1;
+                break;
+            }
+            else if (IsSplitter(next))
+            {
+                lifetimesLeft = DetermineNumberOfDifferentTimelines(next);
+                break;
+            }
+            else
+            {
+                next = next.GetNeighbour(1, 0); // move to down neighbour
+            }
+        }
+
+        // determine number of timelines on the right
+        var rightNeighbour = position.GetNeighbour(0, 1);
+        next = rightNeighbour;
+        UInt128 lifetimesRight;
+        while (true)
+        {
+            if (!next.IsValidPosition) {
+                lifetimesRight = 1;
+                break;
+            }
+            else if (IsSplitter(next))
+            {
+                lifetimesRight = DetermineNumberOfDifferentTimelines(next);
+                break;
+            }
+            else
+            {
+                next = next.GetNeighbour(1, 0); // move to down neighbour
+            }
+        }
+
+        var totalLifetimes = lifetimesLeft + lifetimesRight;
+        _timelinesPerPosition[position] = totalLifetimes;
+        return totalLifetimes;
     }
 }
